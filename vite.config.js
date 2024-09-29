a7b429244364ae8095159c3d711f913b69f39be2
muhammad-danish <totalnetsolution2@gmail.com>
2024-09-29 16:28:41 +0500

    app created

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`,
+      },
+    },
+  },
+  server: {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  },
+});
